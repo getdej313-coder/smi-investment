1257eed79dc0468206398071a46fd3d9362967d7
getdej313-coder <getdej313@gmail.com>
2026-02-21 02:00:08 -0800

    Update official.php

--- official.php
+++ official.php
@@ -1,4 +1,6 @@
-<?php require_once 'includes/auth.php'; ?>
+<?
+require_once 'config/session.php';
+php require_once 'includes/auth.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -393,4 +395,5 @@
         </div>
     </div>
 </body>
+
 </html>
