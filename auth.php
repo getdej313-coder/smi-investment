bb9e85ed640f7166922b7eeab0ee8d261c172fd8
getdej313-coder <getdej313@gmail.com>
2026-02-21 03:20:19 -0800

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,12 +1,12 @@
 <?php
-// includes/auth.php - ULTRA SIMPLE
-// DO NOT START SESSION HERE - already started in config/session.php
+// Always start session safely
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 // If no user_id, redirect to login
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
 }
-
-// If we get here, user is logged in - do nothing else
 ?>
